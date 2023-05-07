66ec103c901be147d037ba41871155cf9b29fc3a
lydid0u <122122182+lydid0u@users.noreply.github.com>
2023-05-08 01:11:02 +0200

    un bon debut

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboudjel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 00:57:46 by lboudjel          #+#    #+#             */
+/*   Updated: 2023/05/08 00:57:46 by lboudjel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+#include <stdarg.h>
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+int		ft_printf(const char *str, ...);
+int	print_c(va_list args);
+
+
+void	ft_putchar(char c);
+int		ft_putstr(const char *str);
+void	ft_putnbr(int n);
+void	ft_putnbr_positive(unsigned int n);
+void	ft_putnbr_base(unsigned long long int nb, char *base);
+
+#endif
